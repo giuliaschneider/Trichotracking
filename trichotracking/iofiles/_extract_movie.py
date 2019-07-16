20022c90534443695f0bce7128afd75408fb561a
giuliaschneider <giuliasc@student.ethz.ch>
2019-07-16 13:26:28 +0200

    Adds dfparticles, nTracks, filename as kw-arguments to iofiles.extract_movie.exportmovie

--- trichotracking/iofiles/_extract_movie.py
+++ trichotracking/iofiles/_extract_movie.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-from ._list_files import find_files
+from ._list_files import find_files, find_img
 from ._metadata import getTime_timestamp
 from ._image import loadImage
 from geometry import cropRectangleKeepSize
@@ -15,6 +15,25 @@ from geometry import cropRectangleKeepSize
 from IPython.core.debugger import set_trace
 
 __all__ = ['export_movie', 'export_movie_track']
+
+
+def write_particles(img, dfparticles, frame):
+        cx = dfparticles[dfparticles.frame == frame].cx
+        cy = dfparticles[dfparticles.frame == frame].cy
+        trackNr = dfparticles[dfparticles.frame == frame].trackNr   
+        for ccx, ccy, nr in zip(cx, cy, trackNr):
+            ccx = int(ccx)
+            ccy = int(ccy)   
+            cv2.circle(img, (ccx, ccy), (3), (255, 255, 255))
+            font                   = cv2.FONT_HERSHEY_DUPLEX
+            bottomLeftCornerOfText = (ccx+10,ccy+10)
+            fontScale              = 3
+            fontColor              = (255, 255, 255)
+            txt = "{:.0f}".format(nr)
+            cv2.putText(img, txt, bottomLeftCornerOfText,
+                font, fontScale, fontColor)
+            
+
 
 
 def write_time(img, time, width, scale=1):
@@ -28,18 +47,16 @@ def write_time(img, time, width, scale=1):
         font, fontScale, fontColor)
 
 
-def export_movie(dir, fps=20):
+def export_movie(dir, fps=20, dfparticles=None, nTracks=None,
+                 filename=None):
     """ Saves movie to parent directory of dir. """
     # Get files, directories
-    listOfFiles = find_files(dir)
-    #saveDir, _ = os.path.split(dir)
-    saveDir, filename = os.path.split(dir)
-    print(saveDir)
-    print(filename)
-    filename = os.path.join(saveDir, filename + '.avi')
+    listOfFiles = find_img(dir)
+
+    filename = os.path.join(dir,  'animation.avi')
 
     # Get image size
-    img, height, width= loadImage(listOfFiles[0])
+    img = loadImage(listOfFiles[0])[0]
     size =  (img.shape[1] ,img.shape[0])
 
     # Create viedo writer
@@ -50,11 +67,13 @@ def export_movie(dir, fps=20):
 
     for frame in range(0, len(listOfFiles)):
         # Write the frame into the file 'output.avi'
-        img, height, width= loadImage(listOfFiles[frame])
+        img, _, width = loadImage(listOfFiles[frame])
         if isinstance(img, int) and (img == -1):
             continue
         time = getTime_timestamp(listOfFiles[frame])
         write_time(img, time, width, scale=6)
+        if dfparticles is not None:
+            write_particles(img, dfparticles, frame)
         if img is None:
             set_trace()
         out.write(img)
@@ -74,9 +93,9 @@ def export_movie_track(df, listOfFiles, listTimes, filename, fps, track,
     mult = 1.1
 
     # Get image shape
-    img, height, width= loadImage(listOfFiles[0])
+    img = loadImage(listOfFiles[0])[0]
     crop_params = bx, by, bw, bh, mult
-    cropped, nbx, nby = cropRectangleKeepSize(img, *crop_params)
+    cropped = cropRectangleKeepSize(img, *crop_params)[0]
     size =  (cropped.shape[1] ,cropped.shape[0])
 
     times = np.array([datetime.fromtimestamp(i) for i in listTimes])
@@ -94,9 +113,9 @@ def export_movie_track(df, listOfFiles, listTimes, filename, fps, track,
 
     for frame in range(startFrame, endFrame):
         print(frame)
-        img, height, width= loadImage(listOfFiles[frame])
+        img = loadImage(listOfFiles[frame])[0]
         crop_params = bx, by, bw, bh, mult
-        cropped, nbx, nby = cropRectangleKeepSize(img, *crop_params)
+        cropped  = cropRectangleKeepSize(img, *crop_params)[0]
 
         write_time(cropped, times[frame], bw)
 
@@ -104,7 +123,6 @@ def export_movie_track(df, listOfFiles, listTimes, filename, fps, track,
         if((dark is not None) and (not dark[frame])):
             cv2.circle(cropped, (bw-50,60), 5, (255), -1)
 
-        cropped = cropped.astype(np.uint8)
         size =  (cropped.shape[1] ,cropped.shape[0])
 
         # Write the frame into the file 'output.avi'
