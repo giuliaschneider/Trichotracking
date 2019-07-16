256b953f3f9549363b01eb305e763c85c27c2abc
giuliaschneider <giuliasc@student.ethz.ch>
2019-07-16 09:01:35 +0200

    Fixes imports

--- trichotracking/segmentation/chamber_func.py
+++ trichotracking/segmentation/chamber_func.py
@@ -1,3 +1,7 @@
+import numpy as np
+import cv2
+
+
 def calc_chamber(background):
     """ Get bw image, pixels inside set to 255, outside 0. """
     bw = cv2.adaptiveThreshold(background, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
@@ -19,7 +23,6 @@ def calc_chamber_df_ulisetup(background):
     ret, bw = cv2.threshold(background,200,255,cv2.THRESH_BINARY)
     h, w = bw.shape[:2]
     mask = np.zeros((h+2, w+2), np.uint8)
-    set_trace()
     cv2.floodFill(bw, mask, (int(w/2),int(h/2)), 255)
     mask[mask==1] = [255]
     return mask[1:-1,1:-1]
