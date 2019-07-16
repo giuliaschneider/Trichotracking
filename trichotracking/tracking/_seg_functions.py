55bd99079e5f3c187dfd0bd01be15855ca8bfab3
giuliaschneider <giuliasc@student.ethz.ch>
2019-07-16 13:29:00 +0200

    refactors tracking._seg_functions

--- trichotracking/tracking/_seg_functions.py
+++ trichotracking/tracking/_seg_functions.py
@@ -5,13 +5,23 @@ from plot.plot_images import plotAllImages
 from IPython.core.debugger import set_trace
 
 
-def segementTrichosBlurred(img, background=None, plotImages=False,
-                           threshold=28, chamber=None):
-    """ Segments image and saves objects to self.contours. """
-    blurred = cv2.GaussianBlur(img,(5, 5),0)
-    subtracted = cv2.subtract(cv2.bitwise_not(blurred),
-                 cv2.bitwise_not(background))
-    ret, bw = cv2.threshold(subtracted, threshold, 255, cv2.THRESH_BINARY)
+
+def segment_particles(inimg, background, chamber=None, blur=True, 
+                      darkField=False, plotImages=False, threshold=28):
+    """ Segments image and returns image and semgmented image. """
+    img = inimg
+    if blur:
+        img = cv2.GaussianBlur(img,(5, 5),0)
+    if darkField:
+        subtracted = cv2.subtract((img), (background))
+        bw = cv2.adaptiveThreshold(cv2.bitwise_not(subtracted),255, \
+              cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,31,4)
+    else:
+        subtracted = cv2.subtract(cv2.bitwise_not(img),
+                                  cv2.bitwise_not(background))
+        _, bw = cv2.threshold(subtracted, threshold, 255, cv2.THRESH_BINARY)
+
+    # Close gaps by morphological closing
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
     bw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
 
@@ -23,42 +33,3 @@ def segementTrichosBlurred(img, background=None, plotImages=False,
     return img, bw
 
 
-def segementTrichosLight(filepath, background=None, plotImages=False):
-    """ Segments image and saves objects to self.contours. """
-    subtracted = cv2.subtract(cv2.bitwise_not(img),
-                              cv2.bitwise_not(background))
-    ret, bw = cv2.threshold(subtracted, 17, 255, cv2.THRESH_BINARY)
-    # Close gaps by morphological closing
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    bw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-    # plot images
-    if plotImages:
-        imgs = [img, bw, background]
-        labels = ["Original", "Threshold", "bg"]
-        plotAllImages(imgs, labels)
-    return img, bw
-
-
-def segementTrichosDarkField(img, background=None, plotImages=False,
-                             chamber=None, threshold=11):
-    """ Segments image and saves objects to self.contours. """
-    blurred = cv2.GaussianBlur(img,(5, 5),0)
-    substracted = cv2.subtract((blurred), (background))
-
-    if chamber is not None:
-        substracted[chamber==0] = 0
-
-    bw = cv2.adaptiveThreshold(cv2.bitwise_not(substracted),255, \
-                cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,31,4)
-    # Close gaps by morphological closing
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    bw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-
-    bw = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
-
-    if plotImages:
-        imgs = [img, bw]
-        labels = ["Original", "Threshold"]
-        plotAllImages(imgs, labels)
-    return img, bw
