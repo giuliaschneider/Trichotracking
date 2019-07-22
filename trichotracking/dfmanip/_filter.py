4598dfdc244cc5630d026a2d553a424f375fc671
giuliaschneider <giuliasc@student.ethz.ch>
2019-07-22 10:03:31 +0200

    adds dfmanip._filter

---
+++ trichotracking/dfmanip/_filter.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+__all__ = ['removeNan']
+
+
+def removeNan(df, cols):
+    """ Returns dataframe view, all NaN-rows in cols removed.
+
+    Parameters
+    ----------
+    df : pandas Dataframe
+    cols: list of str
+        columns which are filtered for NaNs 
+
+    Returns
+    -------
+    d : pandas Dataframe
+        Filtered dataframe
+
+    """
+    if(isinstance(cols, 'str')):
+        cols = [cols]
+    
+    d = df
+    for col in cols:
+        d = d[~d[col].isin([np.inf, -np.inf, np.nan])]
+    return d
