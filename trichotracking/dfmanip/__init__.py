a0b4655b6b86ae37a1ff3449f5e45e0777ef8759
giuliaschneider <giuliasc@student.ethz.ch>
2019-07-22 10:03:51 +0200

    moves fit from plot to dfmanip

--- trichotracking/dfmanip/__init__.py
+++ trichotracking/dfmanip/__init__.py
@@ -3,6 +3,8 @@
 
 from ._columns import *
 from ._conversion import *
+from ._filter import *
+from ._fit import *
 from ._group import *
 from ._label import *
 from ._ma import *
@@ -14,6 +16,8 @@ from ._velocity import *
 __all__ = []
 __all__.extend(_columns.__all__)
 __all__.extend(_conversion.__all__)
+__all__.extend(_filter.__all__)
+__all__.extend(_fit.__all__)
 __all__.extend(_group.__all__)
 __all__.extend(_label.__all__)
 __all__.extend(_ma.__all__)
---
+++ trichotracking/dfmanip/_fit.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+from ._filter import removeNan
+
+
+__all__ = ['fit_log', 'fit_semilog']
+
+
+
+def fit_log(df, xcol, ycol):   
+    """
+    Linearly fits log(x) to log(y) and returns coefficients.
+
+    Parameters
+    ----------
+    df : pandas Dataframe
+    xcol: str
+        name of x-column
+    ycol: str
+        name of y-column
+
+    Returns
+    -------
+    coeff : tuple
+        Coefficients (p0, p1): log(y) = p0*log(x) + p1
+
+    """
+    dview = df[[xcol, ycol]]
+    xcol_log = xcol + '_log'
+    ycol_log = ycol + '_log'
+    dview[xcol_log] = np.log(dview[xcol])
+    dview[ycol_log] = np.log(dview[ycol])
+
+    coeff = np.polyfit(np.log(dview[xcol].values), np.log(dview[ycol].values), 1)
+    return coeff
+
+
+def fit_semilog(df, xcol, ycol):
+    """
+    Linearly fits x to log(y) and returns coefficients.
+
+    Parameters
+    ----------
+    df : pandas Dataframe
+    xcol: str
+        name of x-column
+    ycol: str
+        name of y-column
+
+    Returns
+    -------
+    coeff : tuple
+        Coefficients (p0, p1): log(y) = p0*log(x) + p1
+
+    """
+    dview = df[[xcol, ycol]]
+    ycol_log = ycol + '_log'
+    dview[ycol_log] = np.log(dview[ycol])
+
+    coeff = np.polyfit( dview[xcol].values , np.log( dview[ycol].values ), 1)
+    return coeff
