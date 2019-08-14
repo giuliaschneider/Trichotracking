8902b66437aaaaee0f3295e13536d793603c300d
giuliaschneider <giuliasc@student.ethz.ch>
2019-08-14 10:26:51 +0200

    adds calculate methods

--- trichotracking/postprocessing/postprocessor.py
+++ trichotracking/postprocessing/postprocessor.py
@@ -1,23 +1,41 @@
 from os.path import join
+from pdb import set_trace
 
 from plot import hist_oneQuantity, ts_oneQuantity
 
-from pdb import set_trace
 
 class Postprocesser:
 
-    def __init__(self, trackkeeper, aggkeeper, pairkeeper, destDir):
+    def __init__(self, trackkeeper, aggkeeper, pairtrackskeeper, listTimes, destDir, pxConversion):
         self.trackkeeper = trackkeeper
         self.aggkeeper = aggkeeper
-        self.pairkeeper = pairkeeper
+        self.pairtrackskeeper = pairtrackskeeper
+        self.listTimes = listTimes
+        self.dest = destDir
+        self.pxConversion = pxConversion
+
         self.singleTrackNrs = trackkeeper.getTrackNrSingles()
         self.pairTrackNrs = trackkeeper.getTrackNrPairs()
-        self.dest = destDir
+
+        self.calculate_trackkeeper()
+        self.calculate_pairtrackkeeper()
+        self.trackAverageProperties()
 
         self.saveHist_vSingle()
         self.save_ts_vFrame()
         self.generateOverview()
-        self.trackAverageProperties()
+
+    def calculate_trackkeeper(self):
+        self.trackkeeper.setTime(self.listTimes)
+        self.trackkeeper.setLabel()
+        self.trackkeeper.calcLengthVelocity(self.pxConversion)
+        self.trackkeeper.calcReversals()
+
+    def calculate_pairtrackkeeper(self):
+        self.pairtrackskeeper.setTime(self.listTimes)
+        self.pairtrackskeeper.setLabel()
+        self.pairtrackskeeper.calcLengthVelocity(self.pxConversion)
+        self.pairtrackskeeper.calcReversals(self.pxConversion)
 
     def trackAverageProperties(self):
         dfTracks = self.trackkeeper.getDf()
@@ -26,16 +44,39 @@ class Postprocesser:
         npeaks = dfg.peaks.count()
         self.trackkeeper.addColumnMeta(npeaks.rename('n_reversals').reset_index())
         dt = dfg.time.last() - dfg.time.first()
-        self.trackkeeper.addColumnMeta((npeaks/dt).rename('f_reversal').reset_index())
-        set_trace()
+        self.trackkeeper.addColumnMeta((npeaks / dt).rename('f_reversal').reset_index())
 
+        dfg_nostalling = dfTracks[dfTracks.v_abs > 0.1].groupby('trackNr')
+        self.trackkeeper.addColumnMeta(dfg_nostalling.v_abs.mean().rename('vabs_mean_without_stalling').reset_index())
+        dfg_stalling = dfTracks[dfTracks.v_abs < 0.1].groupby('trackNr')
+        fstalling = dfg_stalling.time.count() / dfg.time.count()
+        self.trackkeeper.addColumnMeta(fstalling.rename('fstalling').reset_index())
+
+        dfPairTracks = self.pairtrackskeeper.getDf()
+        dfgpairs = dfPairTracks.groupby('trackNr')
+        self.pairtrackskeeper.addColumnMeta(dfgpairs.v1_ma.mean().rename('v1_mean').reset_index())
+        self.pairtrackskeeper.addColumnMeta(dfgpairs.v2_ma.mean().rename('v2_mean').reset_index())
+        self.pairtrackskeeper.addColumnMeta(dfgpairs.v_pos_abs.mean().rename('relative_v_mean').reset_index())
+        npeaks = dfgpairs.peaks.count()
+        dt = dfgpairs.time.last() - dfgpairs.time.first()
+        self.pairtrackskeeper.addColumnMeta(npeaks.rename('npeaks').reset_index())
+        self.pairtrackskeeper.addColumnMeta((npeaks/dt).rename('f_reversal').reset_index())
+        self.pairtrackskeeper.addColumnMeta(dfgpairs.lol_reversals.mean().rename('lol_reversals_mean').reset_index())
+        self.pairtrackskeeper.addColumnMeta(dfgpairs.lol_reversals_normed.mean().rename('lol_reversals_normed_mean').reset_index())
+        totallength = dfgpairs.l1_um.mean() + dfgpairs.l2_um.mean()
+        self.pairtrackskeeper.addColumnMeta(totallength.rename('total_length').reset_index())
+
+        dfg_nostalling = dfPairTracks[dfPairTracks.v_pos_abs > 0.1].groupby('trackNr')
+        self.pairtrackskeeper.addColumnMeta(dfg_nostalling.v_pos_abs.mean().rename('rel_v_mean_without_stalling').reset_index())
+        dfg_stalling = dfPairTracks[dfPairTracks.v_pos_abs < 0.1].groupby('trackNr')
+        fstalling = dfg_stalling.time.count() / dfg.time.count()
+        self.pairtrackskeeper.addColumnMeta(fstalling.rename('fstalling').reset_index())
 
     def generateOverview(self):
         file = join(self.dest, 'exp_overview.txt')
         with open(file, 'w') as f:
             f.write('# single tracks: {:d}'.format(self.singleTrackNrs.size))
             f.write('# pair tracks: {:d}'.format(self.pairTrackNrs.size))
-
 
     def saveHist_vSingle(self):
         df = self.trackkeeper.getDf()
