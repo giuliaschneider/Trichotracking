29cc9aaa655c06f3e0fe363aaecfcdbb2084741d
giuliaschneider <giuliasc@student.ethz.ch>
2019-08-14 10:27:05 +0200

    adds pairtrackkeeper

--- trichotracking/trackkeeper/__init__.py
+++ trichotracking/trackkeeper/__init__.py
@@ -1,4 +1,5 @@
 from .aggkeeper import Aggkeeper
 from .pairkeeper import Pairkeeper
+from .pairtrackkeeper import Pairtrackkeeper
 from .trackkeeper import Trackkeeper
 from .trackmeta import Trackmeta
