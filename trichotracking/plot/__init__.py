2025f7e2694435e3a672bf156c8236adea9bb6e4
giuliaschneider <giuliasc@student.ethz.ch>
2019-08-15 10:31:48 +0200

    adds plotting functions

--- trichotracking/plot/__init__.py
+++ trichotracking/plot/__init__.py
@@ -1,13 +1,19 @@
 from ._constants import *
 from ._draw import *
 from ._hist import *
+from ._lolhist import *
 from ._save import *
+from ._thist import *
 from ._ts import *
+from ._vhist import *
 from .plot_images import *
 
 __all__ = []
 __all__.extend(_draw.__all__)
 __all__.extend(_hist.__all__)
+__all__.extend(_lolhist.__all__)
 __all__.extend(_save.__all__)
+__all__.extend(_thist.__all__)
 __all__.extend(_ts.__all__)
+__all__.extend(_vhist.__all__)
 __all__.extend(plot_images.__all__)
