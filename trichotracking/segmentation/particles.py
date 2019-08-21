da88af4fc0c054dd4d2c12a707339508e8ec0758
giuliaschneider <giuliasc@student.ethz.ch>
2019-08-21 15:43:02 +0200

    refactor code

--- trichotracking/segmentation/particles.py
+++ trichotracking/segmentation/particles.py
@@ -1,27 +1,26 @@
-import numpy as np
 import multiprocessing as mp
-import pandas as pd
-import matplotlib.pyplot as plt
+
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from trichotracking.iofiles import (find_img,
+                                    getTime,
+                                    loadImage)
+from trichotracking.plot import plotAllImages, plotAllContoursTracking
+from trichotracking.regionprops import Contour, insideROI
 
-from iofiles import (find_img,
-                     getTime,
-                     loadImage)
-from plot.plot_images import plotAllImages, plotAllContoursTracking
-from regionprops import Contour, insideROI
-
-
-flags  = ["area", "angle", "bounding_box", "centroid", "contours", "eccentricity", 
-          "eigen", "length", "min_box", "min_rect_angle", "orientation", 
-          "solidity"]
+flags = ["area", "angle", "bounding_box", "centroid", "contours", "eccentricity",
+         "eigen", "length", "min_box", "min_rect_angle", "orientation",
+         "solidity"]
 
 
 def filterParticlesArea(img, bw, roi=None):
     allObjects = Contour(img, bw, flags=flags, cornerImage=None)
     particles = allObjects.particles
-    particles = particles[((particles.area>150)
-                          &(particles.area<5000)
-                          )]
+    particles = particles[((particles.area > 50)
+                           & (particles.area < 5000)
+                           )]
     if roi is not None:
         inside = insideROI(particles.min_box.values, roi)
         particles = particles[inside]
@@ -42,39 +41,35 @@ def getParticleList(particles, vCurrentIndex, frame, vTrackNr):
 
 def particles_image(file,
                     frame,
-                    background, 
+                    background,
                     findingFunction,
-                    chamber=None, 
+                    chamber=None,
                     blur=True,
-                    darkField=False, 
-                    plotImages=False, 
+                    darkField=False,
+                    plotImages=False,
                     threshold=28):
     """ Segments image and returns property list of found particles. """
 
     # Segment image(file)
     img = loadImage(file)[0]
     if blur:
-        img = cv2.GaussianBlur(img,(3, 3),0)
+        img = cv2.GaussianBlur(img, (3, 3), 0)
 
     if darkField:
-        subtracted = cv2.subtract((img), (background))
+        subtracted = cv2.subtract(img, background)
         _, bw = cv2.threshold(subtracted, threshold, 255, cv2.THRESH_BINARY)
     else:
         subtracted = cv2.subtract(cv2.bitwise_not(img),
                                   cv2.bitwise_not(background))
         _, bw = cv2.threshold(subtracted, threshold, 255, cv2.THRESH_BINARY)
 
-
-
     if chamber is not None:
         bw[chamber == 0] = [0]
 
     # Close gaps by morphological closing
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
     bw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-    #bw = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
-
-
+    # bw = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
 
     # Get particles
     particles = findingFunction(img, bw, chamber)
@@ -97,43 +92,42 @@ def particles_image(file,
     return particleList, time
 
 
-def particles_sequence( input_dir,
-                        output_dir,
-                        pxLength,
-                        maxR,
-                        findingFunction,
-                        background,
-                        linkTime=1,
-                        minLength=1,
-                        plotImages=False,
-                        threshold=28,
-                        roi=None,
-                        blur=True,
-                        darkField=False):
-    
+def particles_sequence(input_dir,
+                       output_dir,
+                       pxLength,
+                       maxR,
+                       findingFunction,
+                       background,
+                       linkTime=1,
+                       minLength=1,
+                       plotImages=False,
+                       threshold=28,
+                       roi=None,
+                       blur=True,
+                       darkField=False):
     listOfFiles = find_img(input_dir)
     nFrames = len(listOfFiles)
     listOfFrames = list(range(nFrames))
 
     pool = mp.Pool(processes=4)
-    results = [pool.apply_async(particles_image, 
-                args=(  
-                    listOfFiles[x],
-                    listOfFrames[x],
-                    background,
-                    findingFunction,
-                    roi, 
-                    blur,
-                    darkField,
-                    plotImages,
-                    threshold)) for x in range(nFrames)]
-    
+    results = [pool.apply_async(particles_image,
+                                args=(
+                                    listOfFiles[x],
+                                    listOfFrames[x],
+                                    background,
+                                    findingFunction,
+                                    roi,
+                                    blur,
+                                    darkField,
+                                    plotImages,
+                                    threshold)) for x in range(nFrames)]
+
     pool.close()
     pool.join()
 
     particleList = []
-    for result in results: 
+    for result in results:
         particleList += result.get()[0]
-    imgTimes = [result.get()[1] for result in results] 
+    imgTimes = [result.get()[1] for result in results]
 
     return pd.DataFrame(particleList), np.array(imgTimes)
