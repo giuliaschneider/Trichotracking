89aab500c9d75663645ba5af27161a436ca7d078
giuliaschneider <giuliasc@student.ethz.ch>
2019-08-26 16:56:12 +0200

    optimizes groupdf

--- trichotracking/dfmanip/_group.py
+++ trichotracking/dfmanip/_group.py
@@ -1,13 +1,15 @@
 __all__ = ['groupdf', 'reset_col_levels']
 
 
-def groupdf(df, groupcol='trackNr', aggfunc=['mean', 'std', 'count']):
+def groupdf(df, groupcol='trackNr', aggfunc=['mean', 'std']):
     """ Return grouped dataframe. """
-    dfg = df.groupby(groupcol).agg(aggfunc)
-    dfg.reset_index(inplace=True)
-    dfg.fillna(0, inplace=True)
-    reset_col_levels(dfg)
-    return dfg
+    dfg = df.groupby(groupcol)
+    agg = dfg.agg(aggfunc)
+    agg.reset_index(inplace=True)
+    agg.fillna(0, inplace=True)
+    reset_col_levels(agg)
+    agg['nFrames'] = dfg.count().max(axis=1).values
+    return agg
 
 
 def reset_col_levels(dfg):
