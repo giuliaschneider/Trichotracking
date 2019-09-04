faa79609af279703f254223d7449442851305aea
giuliaschneider <giuliasc@student.ethz.ch>
2019-09-04 13:46:45 +0200

    handles relative paths

--- bin/preprocess/move_files.sh
+++ bin/preprocess/move_files.sh
@@ -2,28 +2,22 @@
 
 # Data directory is passed as argument
 args=( $@ );
-datadir=$1; echo $datadir
-dirs=${args[@]:1}; echo "${dirs[@]}"
-
-
+parentdir=$1;
+dirs=${args[@]:1};
 
 # Save directories
-cd "$datadir"; cd ..
-parentdir=$(pwd)
-datadir="$parentdir/data"
-
+cd "$parentdir"
+datadir="$(pwd)/data"
 
 mkdir ${dirs[@]}
 ndirs="${#args[@]}"
 ndirs=$(( ndirs-1 ))
-
 
 i=0
 d=1
 
 # Iterate pictures
 cd "$datadir"
-echo "$(pwd)"
 files=(*.JPG)
 imax="${#files[@]}"
 
@@ -35,16 +29,13 @@ for file in "${files[@]}"; do
   if [ $i -lt $imax ]; then
    t1=$(date -r "${files[$i]}" +"%s");
    diff=$(($t1 - $t0))
-   echo $diff
    if [ $diff -gt 1 ]; then
      subdir="${args[$d]}"
-     echo $d
-     echo $subdir
      dir="$parentdir/$subdir"
      cd "$datadir"
      mv *$file "$dir"
      echo "$file" "$dir"
-     d=$(( (d+1) % $ndirs + 1 ))
+     d=$(( d % $ndirs + 1 ))
    fi
   fi
 done
